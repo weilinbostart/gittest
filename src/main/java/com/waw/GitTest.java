d8ac85959a64a359e2b53368b789e8fe49240c7f
weilinbo <weilinbo@atguigu.com>
2021-06-01 20:07:18 +0800

    master commit

--- src/main/java/com/waw/GitTest.java
+++ src/main/java/com/waw/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("second commit");
         System.out.println("third commit");
         System.out.println("hot-fix test");
+        System.out.println("master commit");
     }
 }
