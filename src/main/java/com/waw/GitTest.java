66e413e64f78714b5271525c16ab4aacf17041a1
weilinbo <weilinbo@atguigu.com>
2021-06-01 19:59:16 +0800

    first commit

---
+++ src/main/java/com/waw/GitTest.java
@@ -0,0 +1,14 @@
+package com.waw;/**
+ * @author xiaowei
+ * @create 2021-06-01 19:52
+ */
+
+/**
+ *@ClassName GitTest
+ *@Description TODO
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("this is git test");
+    }
+}
