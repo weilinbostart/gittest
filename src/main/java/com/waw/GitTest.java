b469e767036ee4d2770df17a201333cedcdedc5b
weilinbo <weilinbo@atguigu.com>
2021-06-01 20:01:06 +0800

    second commit

--- src/main/java/com/waw/GitTest.java
+++ src/main/java/com/waw/GitTest.java
@@ -10,5 +10,6 @@ package com.waw;/**
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is git test");
+        System.out.println("second commit");
     }
 }
